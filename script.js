8c7d6a3a3c224d95a6f1c325829ead1e4310a607
Anubhav Jha <anubhawjha893@gmail.com>
2024-04-19 22:49:07 +0530

    chaged

---
+++ script.js
@@ -0,0 +1,19 @@
+
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: .2,
+  });
+
+  
+Shery.textAnimate(".mainhead" /* Element to target.*/, {
+    //Parameters are optional.
+    style:2,
+    y: 10,
+    x:10,
+    delay: 4,
+    duration: 2,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    multiplier: 0.1,
+  });
